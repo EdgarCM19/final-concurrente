4c82d2e6eac7046131b2b880058b855f8fc25f0f
nora2130 <70499983+nora2130@users.noreply.github.com>
2021-04-23 21:25:48 -0500

    saludo

--- Jugador.java
+++ Jugador.java
@@ -4,7 +4,7 @@ public class Jugador implements Proceso {
 
     int id;
     CanalSimple entrada;
-
+//hello Edgar
     public Jugador(int id, CanalSimple entrada){
         this.id = id;
         this.entrada = entrada;
