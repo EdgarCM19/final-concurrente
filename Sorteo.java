3438363acc8b4a8323d19af50507038786777054
nora2130 <nellysar30@yahoo.com.mx>
2021-04-23 22:35:18 -0500

    sorteo listo

--- Sorteo.java
+++ Sorteo.java
@@ -1,5 +1,18 @@
 import Jpim.*;
-
+import java.util.Random;
 public class Sorteo implements Proceso {
-    
+    Integer ganador;
+    CanalSimple salida;
+
+    public Sorteo(CanalSimple salida){
+        this.salida=salida;
+    }
+
+    public void run(){
+        while(true){
+            Random rnd = new Random();
+            n=(int) (rnd.nextDouble() * 21 + 1);
+            salida.send(n);
+        }
+    }
 }
