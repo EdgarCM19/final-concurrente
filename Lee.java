20d8df29e2946c046ad25611448bdc9d06847b22
nora2130 <nellysar30@yahoo.com.mx>
2021-04-23 22:48:54 -0500

    Lee listo

---
+++ Lee.java
@@ -0,0 +1,22 @@
+import Jpmi.*;
+
+public class Lee implements Proceso
+  {
+   CanalSimple canal_in;
+   MsgJugador msg;
+   
+   public Lee(CanalSimple canal_in)
+     {
+        this.canal_in=canal_in;
+     }
+   
+   public MsgJugador getMsg()
+   	{
+        return msg;
+    }
+   
+   public void run()
+     {
+        msg=(MsgJugador)canal_in.receive();
+     }
+  }
