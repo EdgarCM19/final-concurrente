bce02d52bb5d9ce56710813119ae712fde80bafe
nora2130 <nellysar30@yahoo.com.mx>
2021-04-23 22:45:45 -0500

    listo Escribe

--- Escribe.java
+++ Escribe.java
@@ -1,3 +1,19 @@
-public class Escribe {
-    
+import Jpmi.*;
+
+public class Escribe implements Proceso
+{
+ CanalSimple canal_out;
+ MsgJugador msg;
+ 
+ public Escribe(CanalSimple canal_out, MsgJugador msg)
+   {
+    this.canal_out=canal_out;
+    this.msg=msg;
+   }	
+ 
+ public void run()
+   {
+    canal_out.send(msg);
+   }
 }
+
