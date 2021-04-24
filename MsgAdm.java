7a1f14395ec5f18d638d06e81ea497b77d1aa00e
Edgar Castro <edgarcm1908@gmail.com>
2021-04-23 21:32:15 -0500

    Prueba 2

--- MsgAdm.java
+++ MsgAdm.java
@@ -1,9 +1,11 @@
 public class MsgAdm {
 
     public MsgJugador [] jugadores;
+    public int numero;
 
-    public MsgAdm(MsgJugador jugadores){
+    public MsgAdm(MsgJugador jugadores, int numero){
         this.jugadores = jugadores;
+        this.numero = numero;
     }
     
 }
