3d44d90bd09d41027af6607109f7745a13d1f6e9
nora2130 <nellysar30@yahoo.com.mx>
2021-04-23 22:28:22 -0500

    jugador listo

--- Jugador.java
+++ Jugador.java
@@ -1,13 +1,28 @@
 import Jpmi.*;
+import java.util.Random;
 
 public class Jugador implements Proceso {
 
     int id;
-    CanalSimple entrada;
-//hello Edgar
-    public Jugador(int id, CanalSimple entrada){
+    CanalSimple salida, entrada;
+    MsgJugador msg;
+
+    public Jugador(int id, CanalSimple salida,CanalSimple entrada){
         this.id = id;
+        this.salida=salida;
         this.entrada = entrada;
+    }
+    public void run(){
+        Random rnd = new Random();
+        int n=0;
+        while(true){
+            n=(int) (rnd.nextDouble() * 21 + 1);
+            msg=new MsgJugador(id,n);
+            salida.send(msg);
+            msg=(MsgJugador)entrada.receive();
+            String cad = (msg.ganador ) ? "Yeii Gane !!!" : "F.. Perdi !!!";
+            System.out.println("[Jugador "+id+"]:>"+cad);
+        }
     }
 
 }
