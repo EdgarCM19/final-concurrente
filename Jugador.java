e8de648d468d538b0669edc316bbadd85805a358
nora2130 <nellysar30@yahoo.com.mx>
2021-04-26 16:05:21 -0500

    jugador listo

--- Jugador.java
+++ Jugador.java
@@ -6,16 +6,18 @@ public class Jugador implements Proceso {
     int id;
     CanalSimple salida, entrada;
     MsgJugador msg;
+    boolean ciclo;
 
-    public Jugador(int id, CanalSimple salida,CanalSimple entrada){
+    public Jugador(int id, CanalSimple salida,CanalSimple entrada,boolean ciclo){
         this.id      = id;
         this.salida  = salida;
         this.entrada = entrada;
+        this.ciclo=ciclo;
     }
     public void run(){
         Random rnd = new Random();
         int n = 0;
-        // while(true){
+         do{
             n = (int) rnd.nextInt(20) + 1;
             System.out.println("[Jugador " + (this.id + 1) + "]>Elegi el numero: " + n);
             msg = new MsgJugador(id,n);
@@ -23,6 +25,6 @@ public class Jugador implements Proceso {
             msg = (MsgJugador)entrada.receive();
             System.out.println("[Jugador " + (this.id + 1) + "]:>" + 
                     ((msg.ganador ) ? "Yeii Gane !!!" : "F.. Perdi !!!"));
-        // }
+        }while(ciclo);
     }
 }
