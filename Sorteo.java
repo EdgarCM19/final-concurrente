510ed1f1335ed9250d8ec43835c07f1b145cc5ea
nora2130 <nellysar30@yahoo.com.mx>
2021-04-26 16:12:09 -0500

    sorteo listo

--- Sorteo.java
+++ Sorteo.java
@@ -2,15 +2,18 @@ import Jpmi.*;
 import java.util.Random;
 public class Sorteo implements Proceso {
     CanalSimple salida;
+    boolean ciclo;
 
-    public Sorteo(CanalSimple salida){
+    public Sorteo(CanalSimple salida,boolean ciclo){
         this.salida = salida;
+        this.ciclo=ciclo;
     }
 
     public void run(){
-        // while(true)
+        do{
             Integer ganador = (Integer)(new Random().nextInt(20)) + 1;
             System.out.println("-------[SORTEO]------\n[Sorteo]>El numero ganador es: " + ganador);
             salida.send(ganador);
+        }while(ciclo);
     }
 }
